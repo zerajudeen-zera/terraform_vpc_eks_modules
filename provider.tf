bd3dae265e4336ecd5062e3c817a9d67837bf84e
zerajudeen-zera <zerajudeen@gmail.com>
2026-01-03 12:02:21 +0530

    added backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
         version = "6.27.0"
     }
   }
+  backend "s3" {
+    bucket = "myterraformbucket"
+    region = "us-east-1"
+    use_lockfile = true
+    key = "terraform/backend/terraform.tfstate"
+  }
 }
 
 provider "aws" {
